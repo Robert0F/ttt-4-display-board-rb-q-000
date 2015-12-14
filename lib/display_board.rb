a0bf8b7f40c4598cc55c326cdb349ef39d04d018
Robert0F <Robert0F@yahoo.com>
2015-12-14 02:26:21 +0000

    nitrous-backup at Mon Dec 14 02:26:21 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["   ","|","   ","|","   ","___________","   ","|","   ","|","   ","___________","   ","|","   ","|","   "]
 def display_board(board)
-
+  puts#{board[0],board[1],board[2],board[3],board[4],\nboard[5],\nboard[6],board[7],board[8],board[9],board[10],\nboard[11],\nboard[12],board[13],board[14],board[15],board[16],board[17]}
   end
